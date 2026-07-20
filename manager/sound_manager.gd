85f893a880cde5087efdfc8979a4face51e263f8
KimPotatoIV <jossceo@gmail.com>
2026-07-20 17:45:52 +0900

    Allow simultaneous audio playback without cutting off
    Refactor sound manager to create a new AudioStreamPlayer for each sound playback instead of reusing a single player. This prevents audio from being interrupted when multiple sounds should play simultaneously (e.g., multiple cells flipping at once). Each player is automatically cleaned up via the finished signal to prevent memory leaks.

--- manager/sound_manager.gd
+++ manager/sound_manager.gd
@@ -2,6 +2,10 @@
 ▶▶▶ sound_manager.gd ◀◀◀
 
 - 효과음 재생을 전담하는 매니저 스크립트
+- 재생할 때마다 AudioStreamPlayer를 새로 생성해서 재생하고,
+  재생이 끝나면 스스로 삭제되도록 만들어
+  여러 셀이 동시에 플립해도 소리가
+  서로 끊기지 않고 겹쳐서 들리게 함
 """
 
 extends Node
@@ -11,20 +15,26 @@ extends Node
 const FLAP_CLACK_STREAM: AudioStream = \
 	preload("res://audio/sfx/flap_clack.wav")
 
-# 소리를 재생할 노드로 미리 씬 트리에 배치하지 않고 코드로 직접 생성해서 붙임
-var sfx_player: AudioStreamPlayer
-
-##################################################
-func _ready() -> void:
-	# AudioStreamPlayer 노드를 코드로 직접 생성해서 자식으로 추가함
-	sfx_player = AudioStreamPlayer.new()
-	add_child(sfx_player)
-
 ##################################################
 """
 - 외부에서 호출하는 함수로, 호출될 때마다
-  flap_clack 사운드를 스트림으로 지정하고 즉시 재생
+  새로운 AudioStreamPlayer를 만들어 flap_clack 사운드를 재생함
+
+- 기존처럼 플레이어 하나를 재사용하지 않는 이유:
+  플레이어가 하나뿐이면 재생 중에 또 play()가 호출될 경우
+  이전 소리가 끊기고 새 소리로 덮어써짐
+  (여러 셀이 동시에 플립할 때 소리가 뚝뚝 끊기는 원인)
+
+- 매번 새로 만들면 각 소리가 서로 독립적으로 끝까지 재생되어
+  동시에 여러 개가 겹쳐 들리는 자연스러운 효과가 남
 """
 func play_flap_clack_sound() -> void:
+	var sfx_player: AudioStreamPlayer = AudioStreamPlayer.new()
+	add_child(sfx_player)
 	sfx_player.stream = FLAP_CLACK_STREAM
+	
+	# finished 시그널이 울리면 스스로를 큐에 넣어 삭제
+	# 매번 새로 만든 노드가 계속 쌓이지 않도록 정리해주는 부분
+	sfx_player.finished.connect(sfx_player.queue_free)
+	
 	sfx_player.play()
